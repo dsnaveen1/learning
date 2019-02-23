67012e6d784c424062bf3811a802ef8d92bef220
dsnaveen1 <dsnaveen1@gmail.com>
2019-02-23 23:59:35 +0530

    image and text input with add class example

--- front_end/App.js
+++ front_end/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View,Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
+    };
+      
     return (
-      <View style={styles.container}>
-       <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text>My project</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text>My Porject </Text> 
+        <Text>Hello {this.props.name}!</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class LotsOfGreetings extends Component {
+ 
+  render() {
+    
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' pic='pic'/>
+        {/*  to add new img pass pic='location'*/ }
+        <Greeting name='Jaina' />      
+        <Greeting name='Valeera' />
+        
+      </View>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
