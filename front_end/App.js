0738c46e37b05cdea8975edfc5e6ceca8a6e4ae0
dsnaveen1 <dsnaveen1@gmail.com>
2019-02-23 23:12:00 +0530

    addinf image

--- front_end/App.js
+++ front_end/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
     return (
       <View style={styles.container}>
+       <Image source={pic} style={{width: 193, height: 110}}/>
         <Text>My project</Text>
       </View>
     );
